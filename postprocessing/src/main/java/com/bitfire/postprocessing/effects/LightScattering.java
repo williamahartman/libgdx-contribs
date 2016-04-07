058a1267ee20f8c3677fbdd036d2f008b1a8279b
Toni Sagrista <tsagrista@ari.uni-heidelberg.de>
2016-04-07 17:29:43 +0200

    Added Light Scattering effect

---
+++ postprocessing/src/main/java/com/bitfire/postprocessing/effects/LightScattering.java
@@ -0,0 +1,280 @@
+/*******************************************************************************
+ * Copyright 2012 tsagrista
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.bitfire.postprocessing.effects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.bitfire.postprocessing.PostProcessor;
+import com.bitfire.postprocessing.PostProcessorEffect;
+import com.bitfire.postprocessing.filters.Bias;
+import com.bitfire.postprocessing.filters.Blur;
+import com.bitfire.postprocessing.filters.Blur.BlurType;
+import com.bitfire.postprocessing.filters.Combine;
+import com.bitfire.postprocessing.filters.Scattering;
+import com.bitfire.postprocessing.utils.PingPongBuffer;
+
+/** Light scattering implementation.
+ * @author Toni Sagrista */
+public final class LightScattering extends PostProcessorEffect {
+	public static class Settings {
+		public final String name;
+
+		public final BlurType blurType;
+		public final int blurPasses; // simple blur
+		public final float blurAmount; // normal blur (1 pass)
+		public final float bias;
+
+		public final float scatteringIntensity;
+		public final float scatteringSaturation;
+		public final float baseIntensity;
+		public final float baseSaturation;
+
+		public final int ghosts;
+		public final float haloWidth;
+
+		public Settings (String name, BlurType blurType, int blurPasses, float blurAmount, float bias, float baseIntensity,
+			float baseSaturation, float scatteringIntensity, float scatteringSaturation, int ghosts, float haloWidth) {
+			this.name = name;
+			this.blurType = blurType;
+			this.blurPasses = blurPasses;
+			this.blurAmount = blurAmount;
+
+			this.bias = bias;
+			this.baseIntensity = baseIntensity;
+			this.baseSaturation = baseSaturation;
+			this.scatteringIntensity = scatteringIntensity;
+			this.scatteringSaturation = scatteringSaturation;
+
+			this.ghosts = ghosts;
+			this.haloWidth = haloWidth;
+		}
+
+		// simple blur
+		public Settings (String name, int blurPasses, float bias, float baseIntensity, float baseSaturation,
+			float scatteringIntensity, float scatteringSaturation, int ghosts, float haloWidth) {
+			this(name, BlurType.Gaussian5x5b, blurPasses, 0, bias, baseIntensity, baseSaturation, scatteringIntensity,
+				scatteringSaturation, ghosts, haloWidth);
+		}
+
+		public Settings (Settings other) {
+			this.name = other.name;
+			this.blurType = other.blurType;
+			this.blurPasses = other.blurPasses;
+			this.blurAmount = other.blurAmount;
+
+			this.bias = other.bias;
+			this.baseIntensity = other.baseIntensity;
+			this.baseSaturation = other.baseSaturation;
+			this.scatteringIntensity = other.scatteringIntensity;
+			this.scatteringSaturation = other.scatteringSaturation;
+
+			this.ghosts = other.ghosts;
+			this.haloWidth = other.haloWidth;
+
+		}
+	}
+
+	private PingPongBuffer pingPongBuffer;
+
+	private Scattering scattering;
+	private Blur blur;
+	private Bias bias;
+	private Combine combine;
+
+	private Settings settings;
+
+	private boolean blending = false;
+	private int sfactor, dfactor;
+
+	public LightScattering (int fboWidth, int fboHeight) {
+		pingPongBuffer = PostProcessor.newPingPongBuffer(fboWidth, fboHeight, PostProcessor.getFramebufferFormat(), false);
+
+		scattering = new Scattering();
+		blur = new Blur(fboWidth, fboHeight);
+		bias = new Bias();
+		combine = new Combine();
+
+		setSettings(new Settings("default", 2, -0.9f, 1f, 1f, 0.7f, 1f, 8, 0.5f));
+	}
+
+	@Override
+	public void dispose () {
+		combine.dispose();
+		bias.dispose();
+		blur.dispose();
+		pingPongBuffer.dispose();
+	}
+
+	/** Sets the positions of the 10 lights in [0..1] in both coordinates **/
+	public void setLightPositions (int nLights, float[] vec) {
+		scattering.setLightPositions(nLights, vec);
+	}
+
+	public void setBaseIntesity (float intensity) {
+		combine.setSource1Intensity(intensity);
+	}
+
+	public void setBaseSaturation (float saturation) {
+		combine.setSource1Saturation(saturation);
+	}
+
+	public void setScatteringIntesity (float intensity) {
+		combine.setSource2Intensity(intensity);
+	}
+
+	public void setScatteringSaturation (float saturation) {
+		combine.setSource2Saturation(saturation);
+	}
+
+	public void setBias (float b) {
+		bias.setBias(b);
+	}
+
+	public void enableBlending (int sfactor, int dfactor) {
+		this.blending = true;
+		this.sfactor = sfactor;
+		this.dfactor = dfactor;
+	}
+
+	public void disableBlending () {
+		this.blending = false;
+	}
+
+	public void setBlurType (BlurType type) {
+		blur.setType(type);
+	}
+
+	public void setSettings (Settings settings) {
+		this.settings = settings;
+
+		// setup threshold filter
+		setBias(settings.bias);
+
+		// setup combine filter
+		setBaseIntesity(settings.baseIntensity);
+		setBaseSaturation(settings.baseSaturation);
+		setScatteringIntesity(settings.scatteringIntensity);
+		setScatteringSaturation(settings.scatteringSaturation);
+
+		// setup blur filter
+		setBlurPasses(settings.blurPasses);
+		setBlurAmount(settings.blurAmount);
+		setBlurType(settings.blurType);
+
+	}
+
+	public void setBlurPasses (int passes) {
+		blur.setPasses(passes);
+	}
+
+	public void setBlurAmount (float amount) {
+		blur.setAmount(amount);
+	}
+
+	public float getBias () {
+		return bias.getBias();
+	}
+
+	public float getBaseIntensity () {
+		return combine.getSource1Intensity();
+	}
+
+	public float getBaseSaturation () {
+		return combine.getSource1Saturation();
+	}
+
+	public float getScatteringIntensity () {
+		return combine.getSource2Intensity();
+	}
+
+	public float getScatteringSaturation () {
+		return combine.getSource2Saturation();
+	}
+
+	public boolean isBlendingEnabled () {
+		return blending;
+	}
+
+	public int getBlendingSourceFactor () {
+		return sfactor;
+	}
+
+	public int getBlendingDestFactor () {
+		return dfactor;
+	}
+
+	public BlurType getBlurType () {
+		return blur.getType();
+	}
+
+	public Settings getSettings () {
+		return settings;
+	}
+
+	public int getBlurPasses () {
+		return blur.getPasses();
+	}
+
+	public float getBlurAmount () {
+		return blur.getAmount();
+	}
+
+	@Override
+	public void render (final FrameBuffer src, final FrameBuffer dest) {
+		Texture texsrc = src.getColorBufferTexture();
+
+		boolean blendingWasEnabled = PostProcessor.isStateEnabled(GL20.GL_BLEND);
+		Gdx.gl.glDisable(GL20.GL_BLEND);
+
+		pingPongBuffer.begin();
+		{
+			// apply bias
+			bias.setInput(texsrc).setOutput(pingPongBuffer.getSourceBuffer()).render();
+
+			scattering.setInput(pingPongBuffer.getSourceBuffer()).setOutput(pingPongBuffer.getResultBuffer()).render();
+
+			pingPongBuffer.set(pingPongBuffer.getResultBuffer(), pingPongBuffer.getSourceBuffer());
+
+			// blur pass
+			blur.render(pingPongBuffer);
+		}
+		pingPongBuffer.end();
+
+		if (blending || blendingWasEnabled) {
+			Gdx.gl.glEnable(GL20.GL_BLEND);
+		}
+
+		if (blending) {
+			Gdx.gl.glBlendFunc(sfactor, dfactor);
+		}
+
+		restoreViewport(dest);
+
+		// mix original scene and blurred threshold, modulate via
+		combine.setOutput(dest).setInput(texsrc, pingPongBuffer.getResultTexture()).render();
+	}
+
+	@Override
+	public void rebind () {
+		blur.rebind();
+		bias.rebind();
+		combine.rebind();
+		pingPongBuffer.rebind();
+	}
+}
---
+++ postprocessing/src/main/java/com/bitfire/postprocessing/filters/Scattering.java
@@ -0,0 +1,131 @@
+/*******************************************************************************
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.bitfire.postprocessing.filters;
+
+import com.bitfire.utils.ShaderLoader;
+
+/** Scattering Light effect.
+ * @see <a href="https://medium.com/community-play-3d/god-rays-whats-that-5a67f26aeac2">https://medium.com/community-play-3d/god-
+ *      rays-whats-that-5a67f26aeac2</a>
+ * @author Toni Sagrista **/
+public final class Scattering extends Filter<Scattering> {
+	// Number of light supported
+	public static int N = 10;
+
+	private float[] lightPositions;
+	private int nLights;
+
+	private float decay = 0.96815f;
+	private float density = 0.926f;
+	private float weight = 0.58767f;
+
+	private int numSamples = 100;
+
+	/// NUM_SAMPLES will describe the rays quality, you can play with
+	int NUM_SAMPLES = 100;
+
+	public enum Param implements Parameter {
+		// @formatter:off
+		Texture("u_texture0", 0), LightPositions("u_lightPositions", 2), NLights("u_nLights", 0), Decay("u_decay",
+			0), Density("u_density", 0), Weight("u_weight", 0), NumSamples("u_numSamples", 0);
+		// @formatter:on
+
+		private String mnemonic;
+		private int elementSize;
+
+		private Param (String mnemonic, int arrayElementSize) {
+			this.mnemonic = mnemonic;
+			this.elementSize = arrayElementSize;
+		}
+
+		@Override
+		public String mnemonic () {
+			return this.mnemonic;
+		}
+
+		@Override
+		public int arrayElementSize () {
+			return this.elementSize;
+		}
+	}
+
+	public Scattering () {
+		super(ShaderLoader.fromFile("screenspace", "lightscattering"));
+		lightPositions = new float[N * 2];
+		rebind();
+	}
+
+	public void setLightPositions (int nLights, float[] vec) {
+		this.nLights = nLights;
+		this.lightPositions = vec;
+		setParam(Param.NLights, this.nLights);
+		setParamv(Param.LightPositions, this.lightPositions, 0, N * 2);
+	}
+
+	public float getDecay () {
+		return decay;
+	}
+
+	public float getDensity () {
+		return density;
+	}
+
+	public float getWeight () {
+		return weight;
+	}
+
+	public int getNumSamples () {
+		return numSamples;
+	}
+
+	public void setDecay (float decay) {
+		this.decay = decay;
+		setParam(Param.Decay, decay);
+	}
+
+	public void setDensity (float density) {
+		this.density = density;
+		setParam(Param.Density, density);
+	}
+
+	public void setWeight (float weight) {
+		this.weight = weight;
+		setParam(Param.Weight, weight);
+	}
+
+	public void setNumSamples (int numSamples) {
+		this.numSamples = numSamples;
+		setParam(Param.NumSamples, numSamples);
+	}
+
+	@Override
+	public void rebind () {
+		// Re-implement super to batch every parameter
+		setParams(Param.Texture, u_texture0);
+		setParams(Param.NLights, this.nLights);
+		setParamsv(Param.LightPositions, lightPositions, 0, N * 2);
+		setParams(Param.Decay, decay);
+		setParams(Param.Density, density);
+		setParams(Param.Weight, weight);
+		setParams(Param.NumSamples, numSamples);
+		endParams();
+	}
+
+	@Override
+	protected void onBeforeRender () {
+		inputTexture.bind(u_texture0);
+	}
+}
